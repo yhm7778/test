cd46c2e6c1c5dc8d6b3c5f674c33f1a414cefc99
Your Name <you@example.com>
2025-12-09 21:31:14 +0900

    Configure CSP to allow Supabase media and scripts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data: https://avjbrsjwyuwruolptqzi.supabase.co;
+              media-src 'self' blob: data: https://avjbrsjwyuwruolptqzi.supabase.co;
+              font-src 'self';
+              connect-src 'self' https://avjbrsjwyuwruolptqzi.supabase.co;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
